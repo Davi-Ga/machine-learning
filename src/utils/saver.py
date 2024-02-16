70df55180655fa88b97be104b38975c4fbb1a52b
Davi-Ga <contato@daviga.dev.br>
2024-02-16 20:02:27 -0300

    Add Saver class to save selected pages as separate PDFs

---
+++ src/utils/saver.py
@@ -0,0 +1,27 @@
+import PyPDF2
+import os
+
+class Saver:
+    def __init__(self, pdf_file, output_folder):
+        self.pdf_file = pdf_file
+        self.output_folder = output_folder
+        self.reader = PyPDF2.PdfReader(self.pdf_file)
+        self.writer = PyPDF2.PdfWriter()
+
+    def _is_page_number_valid(self, page_num):
+        return 1 <= page_num <= len(self.reader.pages)
+
+    def _save_page_as_pdf(self, page_num):
+        self.writer.add_page(self.reader.pages[page_num - 1])
+        output_file = f"{self.output_folder}/page_{page_num}.pdf"
+        if not os.path.exists(self.output_folder):
+            os.makedirs(self.output_folder)
+        with open(output_file, 'wb') as output_pdf:
+            self.writer.write(output_pdf)
+
+    def save_selected_pages(self, pages):
+        for page_num in pages:
+            if not self._is_page_number_valid(page_num):
+                print(f"Page number {page_num} is not valid.")
+                continue
+            self._save_page_as_pdf(page_num)
