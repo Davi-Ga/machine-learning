2b08b29bf097cbc36542df118e9800981b4896cc
Davi-Ga <contato@daviga.dev.br>
2024-02-16 20:02:35 -0300

    Add new dependencies and update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 from utils.extractor import Extractor
-
+from utils.saver import Saver
 
 if __name__ == "__main__":
     extractor = Extractor()
-    print(extractor.extract_info())
+    info,pages=extractor.extract_info()
+    saver = Saver('data/processos/Processo_0020641-26.2020.5.04.0663.pdf', 'data/extracted')
+    saver.save_selected_pages(pages)
+    print(info)
+    print("\nPáginas: ",pages)
+    
