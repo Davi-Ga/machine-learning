d35892e1e28f6266a729a0e3ec56e628f4afdc64
Davi-Ga <contato@daviga.dev.br>
2024-03-05 19:41:50 -0300

    feat: new labels and e ignore images

--- src/utils/labelling.py
+++ src/utils/labelling.py
@@ -3,29 +3,66 @@ import glob
 import pytesseract
 import csv
 
+
 class Labelling:
-    
-    def __init__(self, path='data/tests', search_texts=["ACORDAO"], output_folder='data/labelled'):
+
+    def __init__(
+        self,
+        path="data/cuted",
+        search_texts=[
+            "ACORDAO",
+            "DESPACHO",
+            "SENTENCA",
+            "NOTIFICACAO",
+            "NOTIFICAGAO"
+            "CERTIDAO",
+            "INTIMACAO",
+            "ATA DE AUDIENCIA",
+            "CITACAO",
+            "CITAÇÃO",
+            "CONCILIACAO",
+            "TERMO",
+            "CONSULTA",
+            "DECISAO",
+            "MANDADO",
+            "AVALIACAO",
+        ],
+        output_folder="data/labelled",
+    ):
         self.path = path
         self.output_folder = output_folder
         self.search_texts = search_texts
-        
+
         if not os.path.exists(self.output_folder):
             os.makedirs(self.output_folder)
 
     def create(self):
-        with open(f'{self.output_folder}/labelled_data.csv', 'w', newline='') as file:
+        ignore_images = set()
+        with open(f"{self.output_folder}/labelled_data.csv", "w", newline="") as file:
             writer = csv.writer(file)
             writer.writerow(["Image_Path", "Label"])
-            
-            for image in glob.glob(os.path.join(self.path, '*.png')):
+
+            for image in sorted(glob.glob(os.path.join(self.path, "*.png"))):
+                if image in ignore_images:
+                    continue
+                print(f"{image}")
                 data = pytesseract.image_to_string(image)
-                
+                print(data)
+                found = False
                 for search_text in self.search_texts:
                     if search_text in data:
+                        if search_text == "NOTIFICAGAO":
+                            search_text.replace("G","C")
                         writer.writerow([image, search_text])
-                            
+                        ignore_images.add(image)
+                        found = True
+                        break
+
+                if not found:
+                    writer.writerow([image, ""])
+
+
 if __name__ == "__main__":
     labelling = Labelling()
     labelling.create()
-    print('Identificação das páginas concluída.')
+    print("Identificação das páginas concluída.")
