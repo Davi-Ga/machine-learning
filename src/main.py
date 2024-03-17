fcde6f64e42c1777cf2a426d1ceec5356f0effcd
Davi-Ga <contato@daviga.dev.br>
2024-03-17 10:42:47 -0300

    Update Saver file path and use EXTRACTED_PATH constant

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from utils.extractor import Extractor
 from utils.saver import Saver
+from settings import EXTRACTED_PATH
 
 if __name__ == "__main__":
     extractor = Extractor()
     info,pages=extractor.extract_info()
-    saver = Saver('data/processos/Processo_0021099-48.2017.5.04.0663.pdf', 'data/extracted/process1')
+    saver = Saver('data/processos/Processo_0021280-49.2017.5.04.0663.pdf', EXTRACTED_PATH)
     saver.save_selected_pages(pages)
     print(info)
     print("\nPáginas: ",pages)
